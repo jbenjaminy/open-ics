fa4013a1b2fb8cde6e96a9435ddce7529c68e476
jbenjaminy <young.benjamin@gmail.com>
2016-08-10 17:52:12 -0500

    Adds initial commit

---
+++ js/about.jsx
@@ -0,0 +1,27 @@
+// Intro to the OpenICS™ Project
+
+ 
+
+// Process control operators have the tough job of maintaining production while struggling with rising labor costs and aging infrastructure. At the same time, the worsening threat landscape for these industries won’t let up.  The OpenICS Project directly addresses the challenges facing critical industries with an open-sourced hybrid cloud solution that offers improved services and better security at lowered costs.
+
+ 
+
+// OpenICS is an Infrastructure-as-a-Service design based on well-proven reference architectures, such as the Purdue model. All of the OpenICS components are freely available and ready to integrate into existing networks. Because of the flexibility of OpenStack, your company can set up a pilot utilizing second hand or spare equipment. This is a technology upgrade that reduces manpower and training costs while improving security.
+
+ 
+
+// The benefits of OpenICS include:
+
+//     The PCN operator can move from a capital-intensive infrastructure to an operational model.
+
+//     OpenICS can be implemented incrementally, allowing you to transform at your own pace.
+
+//     Third-party support is abundant in the OpenStack world. It’s easy to get help.
+
+//     Hybrid cloud improves resiliency, and simplifies computing services at the field level.
+
+//     Automation companies like Rockwell, Siemens and Schneider already offer their software in cloud versions on AWS, Rackspace and Azure.
+
+//     The Security Toolkit module is a comprehensive set of tools, built for a driving situational awareness to the facility level.
+
+// The OpenICS Project has focused on Electrical Energy and Oil and Gas initially, because of early adopters in those industries. In planning are models for water treatment and health. We are associated with the Open Automation project. Our current partners are Arista and Rackspace.
---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+
+var webpack = require('webpack');
+
+var packageData = require('./package.json');
+
+var filename = [packageData.name, packageData.version, 'js'];
+
+var babelPresets = {presets: ['react', 'es2015']};
+
+module.exports = {
+  entry:  [
+  'webpack-dev-server/client?http://127.0.0.1:8080',
+  'webpack/hot/only-dev-server',
+  path.resolve(__dirname, packageData.main)
+  ],
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: filename.join('.')
+  },
+  devtool: 'source-map',
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        loaders: ['react-hot', 'babel-loader?'+JSON.stringify(babelPresets)]
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  }
+};
